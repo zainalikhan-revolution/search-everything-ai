c6cbc8ec21448d1d1276e00cc70f2c0a43185ccd
ZAIN Ali Khan <zainsdigitalrevolution@gmail.com>
2025-09-12 18:25:04 +0500

    Update main.tsx

--- search-everything-ai-main/src/main.tsx
+++ search-everything-ai-main/src/main.tsx
@@ -1,5 +1,16 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const root = createRoot(document.getElementById("root")!);
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
